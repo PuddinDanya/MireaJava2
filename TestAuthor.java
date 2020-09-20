58ad336bd4fb15e508bc53952118cdcb1223a26f
PuddinDanya <dkansuzyan@mail.ru>
2020-09-20 15:57:01 +0300

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,8 @@
+package Avtor;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author b1 = new Author("Tolstoy","tolstoy@yandex.ru",'M');
+    System.out.println(b1);
+    }
+}
